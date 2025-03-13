255d4b01f81533935de839829e26afcaa6e51db5
lianhaibo <957377783@qq.com>
2025-03-13 22:14:49 +0800

    feat: 优化 SvgIcon 组件，调整代码格式，确保一致性和可读性

--- src/components/svg-icon.tsx
+++ src/components/svg-icon.tsx
@@ -1,16 +1,20 @@
-const svgFiles = import.meta.glob('../assets/svgs/*.svg', { eager: true }); // 根据你的路径修改
-const Error = svgFiles['../assets/svgs/error.svg']?.ReactComponent
+const svgFiles = import.meta.glob("../assets/svgs/*.svg", { eager: true }); // 根据你的路径修改
+const Error = svgFiles["../assets/svgs/error.svg"]?.ReactComponent;
 
 type SvgProps = {
-  name: string,
-  width?: string,
-  height?: string,
-  className?: string
-}
+  name: string;
+  width?: string;
+  height?: string;
+  className?: string;
+};
 const SvgIcon = ({ name, width = "20", height = "20", className }: SvgProps) => {
-  const Icon = svgFiles[`../assets/svgs/${name}.svg`]?.ReactComponent
-  
-  return Icon ? <Icon className={className} width={width} /> : <Error width={width} height={height} />
-}
+  const Icon = svgFiles[`../assets/svgs/${name}.svg`]?.ReactComponent;
+
+  return Icon ? (
+    <Icon className={className} width={width} height={height} />
+  ) : (
+    <Error width={width} height={height} />
+  );
+};
 
 export default SvgIcon;
